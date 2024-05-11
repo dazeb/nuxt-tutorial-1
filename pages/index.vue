68752f60b6b518148befc4af601243d08729d930
dazeb <daz@dazeb.dev>
2024-05-11 18:35:43 +0100

    basic API created

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,15 @@
 <script setup>
 const { data: scizor } = await useFetch('https://pokeapi.co/api/v2/pokemon/scizor')
 
+const { data } = await useFetch('/api/scizor')
+
 </script>
 
 <template>
     <h1>Hello World - index.vue</h1>
+    <pre>
+        {{ data }}
+    </pre>
     <pre>
         {{ scizor }}
     </pre>
